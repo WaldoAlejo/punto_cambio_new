6172c98c0e16b98931c54a46a3ed49cc9b3ecee1
Oswaldo Cevallos <cevallos.oswaldo@gmail.com>
2025-07-25 15:22:22 -0500

    prisma

--- server/lib/database.ts
+++ server/lib/database.ts
@@ -1,8 +1,11 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.production" }); // Asegura cargarlo si estás en producción manual
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:Esh2ew8p@localhost:5432/punto_cambio",
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
 });
 
 export { pool };
