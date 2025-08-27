e2e161b5f3232ff2b13bcbb8fa300214b542b385
Oswaldo Cevallos <cevallos.oswaldo@gmail.com>
2025-08-27 15:36:29 -0500

    arreglo 2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+// ecosystem.config.cjs
+module.exports = {
+  apps: [
+    {
+      name: "punto-cambio-api",
+      cwd: "/home/cevallos_oswaldo/punto_cambio_new",
+      script: "server-dist/index.js", // ← si compilas a ./server-dist
+      // script: "dist/index.js",              // ← usa esta línea si decides compilar a ./dist
+      instances: 1,
+      exec_mode: "fork",
+      watch: false,
+      time: true,
+      node_args: "--enable-source-maps",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
+    },
+  ],
+};
