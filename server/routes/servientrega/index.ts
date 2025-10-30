b73af65ecd0908790f7c1da96a0723fb2e659727
Oswaldo Cevallos <cevallos.oswaldo@gmail.com>
2025-10-30 10:44:54 -0500

    listar guias

--- server/routes/servientrega/index.ts
+++ server/routes/servientrega/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticateToken } from "../../middleware/auth.js";
 import { productsRouter } from "./products.js";
 import { shippingRouter } from "./shipping.js";
 import { usersRouter } from "./users.js";
@@ -8,6 +9,9 @@ import { anulacionesRouter } from "./anulaciones.js";
 import receiptsRouter from "./receipts.js";
 
 const router = express.Router();
+
+// Aplicar autenticación a TODAS las rutas de servientrega
+router.use(authenticateToken);
 
 // Montar las sub-rutas
 router.use("/", productsRouter);
